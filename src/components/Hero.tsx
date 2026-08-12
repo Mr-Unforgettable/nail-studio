325e6e387f6c1387ec4b0d6fb159abc4fc0b4558
Mr-Unforgettable <abhianvpsingh2024@protonmail.com>
2026-08-12 07:59:22 +0000

    feat: build hero section.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
-    <section>
-      <h2>Hero Section</h2>
+    <section className="flex-1">
+      <div className="mx-auto grid min-h-[90vh] grid-cols-1 items-center lg:grid-cols-2">
+        {/* Content */}
+        <div className="mx-32 px-6 py-6 lg:px-8">
+          <h1 className="text-4xl font-extrabold leading-tight md:text-5xl">
+            Beautiful Nails, <br /> Designed Just for You
+          </h1>
+          <p className="mt-6 text-lg">
+            Elevate your everyday look with nail services focused on artistry,
+            comfort, and clean, luxurious detail.
+          </p>
+          <button
+            type="button"
+            className="mt-8 rounded-md bg-[#B89C85] text-[#242424] hover:bg-[#b58c6b] font-medium px-6 py-3"
+          >
+            Book Now
+          </button>
+        </div>
+        {/* Image */}
+        <div className="relative min-h-100 w-full sm:min-h-125 lg:min-h-0 lg:h-full">
+          <Image
+            src="/images/hero.jpg"
+            alt="Nail artist creating a manicure"
+            fill
+            className="object-cover"
+          />
+        </div>
+      </div>
     </section>
   );
 }
