37c8758b31d0e789e0ae9ac14c4e58a1a5dd44cc
Mr-Unforgettable <abhianvpsingh2024@protonmail.com>
2026-08-12 04:42:11 +0000

    feat: build navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,37 @@
 export default function Navbar() {
   return (
-    <section>
-      <h2>Navbar</h2>
-    </section>
+    <nav className="pr-32 pl-32 border border-gray-800">
+      <div className="m-4 flex items-center justify-between">
+        <h2 className="pl-6 pr-6 pt-3 pb-3 flex items-center border border-gray-800 rounded-md">
+          Logo ✨
+        </h2>
+        <ul className="m-4 flex items-center justify-between gap-8 list-none">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Booking</a>
+          </li>
+          <li>
+            <a href="#">Portfolio</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <button
+          type="button"
+          className="pl-6 pr-6 pt-3 pb-3 rounded-md gap-2.5 bg-[#B89C85] text-[#242424] hover:bg-[#b58c6b] font-medium"
+        >
+          Book Now
+        </button>
+      </div>
+    </nav>
   );
 }
