305cea783c0543e03e6a5d31b0ee6e181c4713d2
Mr-Unforgettable <abhianvpsingh2024@protonmail.com>
2026-08-12 03:30:42 +0000

    chore: initialized nail studio project.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <section>
+      <h2>Navbar</h2>
+    </section>
+  );
+}
---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,7 @@
+export default function Portfolio() {
+    return (
+        <section>
+            <h2>Portfolio</h2>
+        </section>
+    )
+}
