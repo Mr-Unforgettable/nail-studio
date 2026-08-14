76ee069dbbcb9a9411be0c7336f7eb2f5084c719
Mr-Unforgettable <abhianvpsingh2024@protonmail.com>
2026-08-14 07:44:57 +0000

    refactor: removed unnecessary property.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
-    <main className="bg-[#fdf9f6]">
+    <main>
       <Navbar />
       <Hero />
       <Services />
