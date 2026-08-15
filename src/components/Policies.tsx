75f1ae48a9ad2f3d809214230d316a845dd67b4d
Mr-Unforgettable <abhianvpsingh2024@protonmail.com>
2026-08-15 08:12:26 +0000

    chore: initial skeleton for the Policies component.

---
+++ src/components/Policies.tsx
@@ -0,0 +1,31 @@
+export default function Policies() {
+  return (
+    <div>
+      <div>
+        <h1>Policies</h1>
+        <p>
+          Please review our policies before booking as they help ensure
+          fairness, quality service, and a smooth booking process.
+        </p>
+      </div>
+      <div>
+        <ul>
+          <li>
+            All services are by appointment to ensure dedicated time and care.
+          </li>
+          <li>
+            A deposit may be required to secure your booking and will be applied
+            to your service.
+          </li>
+          <li>
+            Please provide at least 24 hours' notice for cancellations or
+            rescheduling.
+          </li>
+          <li>
+            Arrivals more than 10-15 minutes late may need to be rescheduled.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
