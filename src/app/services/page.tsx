d3ea1b82c713112e8894e12fd9ef4aa569d7cecc
Mr-Unforgettable <abhianvpsingh2024@protonmail.com>
2026-08-16 05:47:04 +0000

    chore: add the services content

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,13 +2,14 @@ import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Policies from "@/components/Policies";
+import ServicesMain from "@/components/ServicesMain";
 
 export default function Services() {
   return (
     <div className="flex min-h-dvh flex-col">
       <Navbar />
       <main className="flex-1">
-        <h1>Services</h1>
+        <ServicesMain />
         <Policies />
         <CTA />
         <Footer />
