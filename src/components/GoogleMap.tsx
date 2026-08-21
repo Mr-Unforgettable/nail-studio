f645c2051025d05257a11afe7185859165f36e56
Mr-Unforgettable <abhianvpsingh2024@protonmail.com>
2026-08-21 07:20:53 +0000

    refactor: removed unnecessary style

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,7 +1,7 @@
 export default function GoogleMap() {
   return (
     <section className="bg-[#fdf9f6] px-6 py-16 sm:px-8 lg:px-12 lg:py-24">
-      <div className="mx-auto max-min">
+      <div className="mx-auto">
         <div className="overflow-hidden border border-[#d8cfc8]">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4377.723133863897!2d-94.67191312803112!3d39.117448997386596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1787295828088!5m2!1sen!2sin"
@@ -11,7 +11,7 @@ export default function GoogleMap() {
             allowFullScreen
             loading="lazy"
             referrerPolicy="strict-origin-when-cross-origin"
-          ></iframe>
+          />
         </div>
       </div>
     </section>
